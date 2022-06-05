bd9de85b3bd977e140c0851cf6712e584c8b6e68
babasow <babasow120@gmail.com>
2022-06-05 22:44:39 +0200

    Follow a user

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteUser, getUser, updateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/:id', getUser);
 
-router.put('/:id', updateUser)
+router.put('/:id', updateUser);
 
-router.delete('/:id', deleteUser)
+router.delete('/:id', deleteUser);
+
+router.put('/:id/follow', followUser);
 export default router;
