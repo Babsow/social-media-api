9b5ccffacf416c21fa82c703ae6bea857f6219d6
babasow <babasow120@gmail.com>
2022-06-05 22:48:28 +0200

    Unfollow user

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -92,3 +92,31 @@ export const followUser = async(req, res)=>{
     }
   }
 }
+
+//UnFlowing a user 
+export const unfollowUser = async(req, res)=>{
+  const id = req.params.id;
+
+  const {currentUserId} = req.body
+
+  if(currentUserId = id){
+    res.status(403).json("Action forbidden");
+  }
+  else {
+    try {
+      const followUser = await UserModel.findById(id);
+      const followingUser = await UserModel.findById(currentUserId);
+
+      if(followUser.followers.incluse(currentUserId)){
+        await followUser.updateOne({$pull : {followers: currentUserId}});
+        await followingUser.updateOne({$pull : {following: id}});
+        res.status(200).json("User Unfollowed!")
+      }
+      else {
+        res.status(403).json("You don't  follow this user")
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+}
