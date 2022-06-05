4b4ac03dd11387f608661dbe8338f8295bbba9b2
babasow <babasow120@gmail.com>
2022-06-05 18:47:20 +0200

    Login User done

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,6 +1,7 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
 
+//Registering
 export const registerUser = async(req, res) =>{
   const{username, password, firstname,lastname } = req.body;
 
@@ -15,4 +16,22 @@ export const registerUser = async(req, res) =>{
   } catch (error) {
     res.status(500).json({message: error.message})
   }
+}
+
+//Login 
+
+export const loginUser = async(req, res) => {
+    const {username, password} =req.body;
+    try {
+      const user = await UserModel.findOne({username: username});
+      if(user){
+        const validity = await bcrypt.compare(password,user.password);
+        validity? res.status(200).json(user) : res.status(400).json("Wrong Password")
+      }
+      else {
+        res.status(404).json("User does not exists")
+      }
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
 }
